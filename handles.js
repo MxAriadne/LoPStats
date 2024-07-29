b8ed4adcbe296349140387db9424063dca3b5c20
Freyja Ariadne <123785382+MxAriadne@users.noreply.github.com>
2024-07-29 12:10:20 -0500

    Crap

---
+++ handles.js
@@ -0,0 +1,263 @@
+const handles = {
+    acidic_crystal_spear: {
+        name: "Acidic Crystal Spear",
+        light_mult: 0.75,
+        heavy_mult: 1.3,
+        ch_heavy_mult: 2.2,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 0, 3]
+    },
+    acidic_great_curved_sword: {
+        name: "Acidic Great Curved Sword",
+        light_mult: 1.4,
+        heavy_mult: 1.55,
+        ch_heavy_mult: 2.35,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 1, 2]
+    },
+    big_pipe_wrench: {
+        name: "Big Pipe Wrench",
+        light_mult: 1.55,
+        heavy_mult: 1.85,
+        ch_heavy_mult: 3.1,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    black_steel_cutter_blade: {
+        name: "Black Steel Cutter",
+        light_mult: 1.25,
+        heavy_mult: 1.55,
+        ch_heavy_mult: 2.5,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 0, 3]
+    },
+    blind_mans_double_sided_spear: {
+        name: "Blind-Man's Double-sided Spear",
+        light_mult: 1,
+        heavy_mult: 1.2,
+        ch_heavy_mult: 2.05,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    bone_cutting_sawblade: {
+        name: "Bone-Cutting Sawblade",
+        light_mult: 1.45,
+        heavy_mult: 2.05,
+        ch_heavy_mult: 2.75,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    booster_glaive: {
+        name: "Booster Glaive",
+        light_mult: 1.3,
+        heavy_mult: 1.5,
+        ch_heavy_mult: 2.15,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    bramble_curved: {
+        name: "Bramble Curved",
+        light_mult: 1.05,
+        heavy_mult: 1.3,
+        ch_heavy_mult: 2.2,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    carcass_crystal_axe: {
+        name: "Carcass Crystal Axe",
+        light_mult: 1.3,
+        heavy_mult: 1.35,
+        ch_heavy_mult: 2.45,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 0, 2]
+    },
+    circular_electric_saw: {
+        name: "Circular Electric Saw",
+        light_mult: 1.4,
+        heavy_mult: 1.75,
+        ch_heavy_mult: 2.35,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 0, 3]
+    },
+    city_longspear: {
+        name: "City Longspear",
+        light_mult: 1.2,
+        heavy_mult: 1.5,
+        ch_heavy_mult: 2.25,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    clock_sword: {
+        name: "Clock Sword",
+        light_mult: 1,
+        heavy_mult: 1.1,
+        ch_heavy_mult: 1.8,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    coil_mjolnir: {
+        name: "Coil Mjolnir",
+        light_mult: 1.5,
+        heavy_mult: 1.65,
+        ch_heavy_mult: 3,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    cursed_knight_halberd: {
+        name: "Cursed Knight Halberd",
+        light_mult: 1.2,
+        heavy_mult: 1.5,
+        ch_heavy_mult: 5.1,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    dancers_curved_sword: {
+        name: "Dancer's Curved Sword",
+        light_mult: 1.15,
+        heavy_mult: 1.35,
+        ch_heavy_mult: 2.55,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 2, null]
+    },
+    electric_coil_stick: {
+        name: "Electric Coil Stick",
+        light_mult: 1.15,
+        heavy_mult: 1.35,
+        ch_heavy_mult: 2.2,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 0, 2]
+    },
+    exploding_pickaxe: {
+        name: "Exploding Pickaxe",
+        light_mult: 1.4,
+        heavy_mult: 1.8,
+        ch_heavy_mult: 2.9,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    fire_axe: {
+        name: "Fire Axe",
+        light_mult: 1.15,
+        heavy_mult: 1.5,
+        ch_heavy_mult: 2.3,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    greatsword_of_fate: {
+        name: "Greatsword of Fate",
+        light_mult: 1.25,
+        heavy_mult: 1.55,
+        ch_heavy_mult: 2.4,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    krat_p_baton: {
+        name: "Krat P-Baton",
+        light_mult: 1.1,
+        heavy_mult: 1.45,
+        ch_heavy_mult: 2.25,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    live_puppets_axe: {
+        name: "Live Puppet's Axe",
+        light_mult: 1.65,
+        heavy_mult: 2.25,
+        ch_heavy_mult: 3.25,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    master_chef_knife: {
+        name: "Master Chef Knife",
+        light_mult: 0.95,
+        heavy_mult: 1.25,
+        ch_heavy_mult: 2.4,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    military_shovel: {
+        name: "Military Shovel",
+        light_mult: 1.35,
+        heavy_mult: 1.5,
+        ch_heavy_mult: 2.55,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    pistol_rock_drill: {
+        name: "Pistol Rock Drill",
+        light_mult: 1.2,
+        heavy_mult: 2,
+        ch_heavy_mult: 2.8,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [2, 0, null]
+    },
+    puppet_saber: {
+        name: "Puppet Saber",
+        light_mult: 1.05,
+        heavy_mult: 1.15,
+        ch_heavy_mult: 1.95,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [1, 1, null]
+    },
+    salamander_dagger: {
+        name: "Salamander Dagger",
+        light_mult: 0.9,
+        heavy_mult: 1.2,
+        ch_heavy_mult: 3,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 1, 2]
+    },
+    spear_of_honor: {
+        name: "Spear of Honor",
+        light_mult: 1.4,
+        heavy_mult: 1.6,
+        ch_heavy_mult: 3.3,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    tyrant_murderers_dagger: {
+        name: "Tyrant Murderer's Dagger",
+        light_mult: 0.75,
+        heavy_mult: 1.1,
+        ch_heavy_mult: 2.2,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    },
+    wintry_rapier: {
+        name: "Wintry Rapier",
+        light_mult: 0.85,
+        heavy_mult: 1.2,
+        ch_heavy_mult: 2.25,
+        //order = m, t, a
+        //scaling = [D, C, B, A, S]
+        scaling: [0, 2, null]
+    }
+};
